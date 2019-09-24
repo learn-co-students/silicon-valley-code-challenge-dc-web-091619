ebe4496dff5ad709e964b84560af69cb47408044
github username <github email address>
2019-09-24 09:32:24 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,7 +1,21 @@
-class FundingRound
+########### Join Model
+# --- FundingRound#startup
+# returns the startup object for that given funding round
+# Once a funding round is created, I should not be able to change the startup
+# --- FundingRound#venture_capitalist
+# returns the venture capitalist object for that given funding round
+# Once a funding round is created, I should not be able to change the venture capitalist
+# --- FundingRound#type
+# returns a string that is the type of funding round
+# Examples include: Angel, Pre-Seed, Seed, Series A, Series B, Series C, etc.
+# --- FundingRound#investment
+# returns a number that is the amount invested during this funding round
+# This should be a float that is not a negative number.
 
-    attr_accessor :type
-    attr_reader :startup, :venturecapitalist, :investment
+
+class FundingRound 
+
+    attr_reader :type, :investment, :startup, :venturecapitalist
     @@all = []
     
     def initialize(type, investment)
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,14 +1,45 @@
-class Startup
 
-    attr_accessor :startup, :name, :venturecapitalist, :fundinground
-    attr_reader :founder, :domain
+# --- Startup#domain
+# returns a string that is the startup's domain
+# Once a startup is created, the domain cannot be changed 
+# (ie. A developer working with instances of the Startup class 
+# cannot directly change the domain)
+# --- Startup#pivot
+# given a string of a domain and a string of a name, change the domain 
+# and name of the startup. This is the only public method through which 
+# the domain can be changed.
+# --- Startup.all
+# should return all of the startup instances
+# --- Startup.find_by_founder
+# given a string of a founder's name, returns the first startup whose 
+# founder's name matches
+# --- Startup.domains
+# should return an array of all of the different startup domains
+# --- Startup#sign_contract
+# given a venture capitalist object, type of investment (as a string), 
+# and the amount invested (as a float), creates a new funding round 
+# and associates it with that startup and venture capitalist.
+# --- Startup#num_funding_rounds
+# Returns the total number of funding rounds that the startup has gotten
+# --- Startup#total_funds
+# Returns the total sum of investments that the startup has gotten
+# --- Startup#investors
+# Returns a unique array of all the venture capitalists that have invested 
+# in this company
+# --- Startup#big_investors
+# Returns a unique array of all the venture capitalists that have invested 
+# in this company and are in the Trés Commas club
+
+class Startup 
+
+    attr_reader :name, :founder, :domain
 
     @@all = []
    
     def pivot(name, domain)
-        self.name = name
-        self.domain = domain
-        self
+        @name = name
+        @domain = domain
+        @@all << self
     end
 
     def self.all
