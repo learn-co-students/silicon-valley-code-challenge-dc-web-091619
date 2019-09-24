0b9d0cd93b5c4cb4fadc7b453745cccaa224dbe3
nihaprezz <sergio13prez@gmail.com>
2019-09-23 21:48:03 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,21 @@
 class FundingRound
+    attr_accessor :name, :type, :investment
+    attr_reader :startup, :capitalist
 
+    @@all = []
+
+    def initialize(name, startup, capitalist, type, investment)
+        @name = name
+        @startup = startup
+        @capitalist = capitalist
+        @type = type
+        @investment = investment
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,43 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name 
+        @total_worth = total_worth.to_i
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select { |people| people.total_worth > 1000000000}
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|fundings| fundings.capitalist == self}
+    end
+
+    def portfolio
+        self.funding_rounds.map {|fundings| fundings.startup}.uniq #uniq used to get rid of duplicates if there is any
+    end
+
+    def biggest_investment
+        #returns the largest funding round given by this venture capitalist
+        self.funding_rounds.max_by {|funding| funding.investment}
+    end
+
+    def invested(domainString)
+
+        find_domain =  self.funding_rounds.select do |funding|
+            funding.startup.domain == domainString
+        end
+        
+        find_domain.map {|funding| funding.investment}.sum
+    end
+
 
 end
